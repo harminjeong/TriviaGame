f2b25ace5e554a2ab1e5e6de3edc120734c2af6a
Harmin Jeong <harminjeong@Harmins-MacBook-Pro.local>
2017-12-13 21:47:19 -0500

    initial site file

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var counter = 60;
+
+$("#quiz").hide();
+
+$("button[type='button']").click(function(){
+  $("#quiz").show();
+    setInterval(function() {
+      counter--;
+       if (counter >= 0) {
+          span = document.getElementById("timer");
+          span.innerHTML = counter;
+       }
+       if (counter === 0) {
+          alert("Sorry, time's up!");
+          clearInterval(counter);
+        }
+      }, 1000);
+ });
+});
